7ba9ab3f84c9f4a848f610a46cda04944bf28b3a
StormySilverio05 <stormysilverionunez@gmail.com>
2024-05-03 20:58:31 -0400

    FIrst

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './assets/components/Footer'
+import Header from './assets/components/Header'
+import Principal from './assets/components/Principal'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Principal/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
